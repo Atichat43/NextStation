2c99a2b7bb53fae0e949a7452b4e77e078f4e2d8
Atichat43 <boss_atln@outlook.com>
2017-11-10 21:20:53 +0700

    before npm install google-maps

--- Vue_NextStation/src/components/nextStation/destination/DestMap.vue
+++ Vue_NextStation/src/components/nextStation/destination/DestMap.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
   .div
-    label From, to {{value}} 
-    .ui.fluid.empty.container
-      MapButton(v-for=" number in 10", v-model="value.from_val", :selectedValue="number") {{number}}
+    label From, to comment 
+    GoogleMap(name="example")
+    //.ui.fluid.empty.container
+       MapButton(v-for=" number in 10", v-model="value.from_val", :selectedValue="number") {{number}}
 </template>
 
 
@@ -15,15 +16,9 @@
 
 <script>
 import MapButton from '../shared/MapButton'
+import GoogleMap from './GoogleMap'
 export default {
   name: 'DestMap',
-  components: { MapButton },
-  props: {
-    value: Object
-  },
-  data () {
-    return {
-    }
-  }
+  components: { MapButton, GoogleMap }
 }
 </script>
