650e7d8f6bdb2c685f9fa715302f87d9a415d73f
Atichat43 <boss_atln@outlook.com>
2017-11-10 09:46:56 +0700

    UPDATE ALL

---
+++ Vue_NextStation/src/components/nextStation/shared/Dropdown.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  .ui.dropdown
+    input(type='hidden', name='gender')
+    i.dropdown.icon
+    .default.text Gender
+    .menu
+      .item(data-value='male') Male
+      .item(data-value='female') Female
+</template>
+
+<script>
+export default {
+  name: 'Dropdown',
+  props: {
+    value: {
+      type: [String, Number],
+      required: true
+    }
+  },
+  mounted () {
+    $(this.$el).dropdown({
+      onChange: (value) => {
+        console.log(value)
+        this.$emit('input', value)
+      }
+    }
+    )
+  }  
+}
+</script>
+
