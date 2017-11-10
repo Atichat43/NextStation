0f3a68626ff30d1296224b6fd5770362af088c07
Atichat43 <boss_atln@outlook.com>
2017-11-11 00:06:32 +0700

    google map working !!!

--- Vue_NextStation/src/components/nextStation/destination/DestMap.vue
+++ Vue_NextStation/src/components/nextStation/destination/DestMap.vue
@@ -1,24 +1,35 @@
 <template lang="pug">
   .div
-    label From, to comment 
-    GoogleMap(name="example")
-    //.ui.fluid.empty.container
-       MapButton(v-for=" number in 10", v-model="value.from_val", :selectedValue="number") {{number}}
+    label From, to {{value}}
+    google-map.fluid.map(:center="center", :zoom="7")
+      // MapButton(v-for=" number in 10", v-model="value.from_val", :selectedValue="number") {{number}}
 </template>
 
 
 <style scope>
-  .ui.fluid.empty.container {
-    background-color: black;
-    min-height: 400px;
+  .map {
+    height: 400px;
+    margin: 0 auto;
+    background: gray;
   }
 </style>
 
 <script>
 import MapButton from '../shared/MapButton'
-import GoogleMap from './GoogleMap'
 export default {
   name: 'DestMap',
-  components: { MapButton, GoogleMap }
+  components: { MapButton },
+  props: {
+    value: Object
+  },
+  data () {
+    return {
+      center: {lat: 10.0, lng: 10.0},
+      markers: [
+        {position: {lat: 10.0, lng: 10.0}},
+        {position: {lat: 11.0, lng: 11.0}}
+      ]
+    }
+  }
 }
 </script>
--- Vue_NextStation/src/router/index.js
+++ Vue_NextStation/src/router/index.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Layout from '@/components/Layout'
+import * as VueGoogleMaps from 'vue2-google-maps'
 var VueScrollTo = require('vue-scrollto')
 
 Vue.use(VueScrollTo)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCQZ6RzPn70v3Db6JVTwOqsRlQ6ZRNEj7w',
+    libraries: 'places' // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  }
+})
 
+Vue.component('google-map', VueGoogleMaps.Map)
 Vue.use(Router)
 
 export default new Router({
