2cf79730e73bcfdb783e5a75614a49b4e8abc315
Atichat43 <boss_atln@outlook.com>
2017-11-14 15:18:00 +0700

    new layout success

--- Vue_NextStation/src/components/nextStation/Destination.vue
+++ Vue_NextStation/src/components/nextStation/Destination.vue
@@ -2,31 +2,40 @@
   .ui.middle.aligned.stackable.grid.container
     // Step
     .ui.small.three.steps(ref="stepBar")
-      .step(v-for="(step, index) in steps", :class="getStepClass(index+1)", @click="backStep(index+1)")
+      .step(v-for="(step, index) in steps", :class="getStepClass(index+1)", @click="setStep(index+1)")
         .content
           .title {{ step }}
     // Button
-    .ui.grid
+    .ui.grid(v-show="getMode !== -1")
       .row
-          .ui.button(@click="setMode(1)") BTS Dark Green
-          .ui.button(@click="setMode(2)") BTS Light Green
-          .ui.button(@click="setMode(3)") MRT Blue
-          .ui.button(@click="setMode(4)") MRT Purple
-          .ui.button(@click="setMode(5)") Airport Link
+          .ui.button(@click="setMode(1)", :class="getMode === 1? 'active':''") BTS Dark Green
+          .ui.button(@click="setMode(2)", :class="getMode === 2? 'active':''") BTS Light Green
+          .ui.button(@click="setMode(3)", :class="getMode === 3? 'active':''") MRT Blue
+          .ui.button(@click="setMode(4)", :class="getMode === 4? 'active':''") MRT Purple
+          .ui.button(@click="setMode(5)", :class="getMode === 5? 'active':''") Airport Link
           label From To {{ getDest }}
     // Map
-    .ui.contrainer.mapContrainer#BTSDARKGREEN(v-show="mode === 1")
+    .ui.contrainer.mapContrainer#BTSDARKGREEN(v-show="getMode === 1")
       .ui.grid
         .row
-          .ui.circular.huge.icon.button(v-for="index in 7", @mouseover="statusText = 'ffaafafaf'", @mouseout="statusText = 'index'", @click="updateVal('ffaafafaf')")
+          .ui.circular.huge.icon.button(
+            v-for="(b, index) in btsDarkGreenButton", 
+            @mouseover="statusText = 'b.text'",
+            @mouseout="statusText = 'index'", 
+            @click="setValue(b.value)")
         .row
           .ui.circular.huge.icon.button(v-for="index in 2")
-    .ui.contrainer.mapContrainer#BTSLIGHTGREEN(v-show="mode === 2")
-    .ui.contrainer.mapContrainer#MRTBLUE(v-show="mode === 3")
-    .ui.contrainer.mapContrainer#MRTPURPLE(v-show="mode === 4")
-    .ui.contrainer.mapContrainer#AIRPORTLINK(v-show="mode === 5")
+    .ui.contrainer.mapContrainer#BTSLIGHTGREEN(v-show="getMode === 2")
+    .ui.contrainer.mapContrainer#MRTBLUE(v-show="getMode === 3")
+    .ui.contrainer.mapContrainer#MRTPURPLE(v-show="getMode === 4")
+    .ui.contrainer.mapContrainer#AIRPORTLINK(v-show="getMode === 5")
+    .ui.contrainer#RESULT(v-show="getMode === -1")
+      h1 RESULT
+      label From {{ destination.from_val }}
+      label To {{ destination.to_val }}
+      .ui.fluid.green.button(@click="tryAgain") TRY AGRAIN
     // Status
-    .ui.black.fluid.label.status {{ statusText }}
+    .ui.black.fluid.label.status(v-show="getMode !== -1") {{ statusText }}
     //
       .ten.wide.column
         DestMap(v-model="destination", @selected="updateDestination")
@@ -81,12 +90,23 @@
         steps: ['From', 'To', 'Result'],
         current_step: 1,
         mode: 1,
-        statusText: 'eiei'
+        statusText: 'eiei',
+        btsDarkGreenButton: [
+          {text: 'สยาม', value: 'siam'},
+          {text: 'พระราม', value: 'pharam'},
+          {text: 'ตีกรุง', value: 'kong'}
+        ]
       }
     },
     computed: {
       getDest: function () {
         return this.destination
+      },
+      getMode: function () {
+        return this.mode
+      },
+      getStep: function () {
+        return this.current_step
       }
     },
     methods: {
@@ -109,11 +129,28 @@
           return { disabled: true }
         }
       },
+      setStep (index) {
+        this.current_step = index
+        this.mode = 1
+      },
       setMode (index) {
         this.mode = index
       },
-      updateVal (value) {
-        this.destination.from_val = value
+      setValue (value) {
+        if (this.current_step === 1) {
+          this.destination.from_val = value
+        } else if (this.current_step === 2) {
+          this.destination.to_val = value
+          this.mode = -1
+        } else {
+          alert('out of range')
+        }
+        this.current_step += 1
+      },
+      tryAgain () {
+        this.current_step = 1
+        this.mode = 1
+        this.destination = { from_val: '', to_val: '' }
       }
     }
   }
