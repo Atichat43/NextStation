fc498c3b27262eb272d93065c16bd6cf6dd5de22
Atichat43 <boss_atln@outlook.com>
2017-11-14 14:42:59 +0700

    new layout image with button

--- Vue_NextStation/src/components/nextStation/Destination.vue
+++ Vue_NextStation/src/components/nextStation/Destination.vue
@@ -1,6 +1,33 @@
 <template lang='pug'>
   .ui.middle.aligned.stackable.grid.container
-    .row
+    // Step
+    .ui.small.three.steps(ref="stepBar")
+      .step(v-for="(step, index) in steps", :class="getStepClass(index+1)", @click="backStep(index+1)")
+        .content
+          .title {{ step }}
+    // Button
+    .ui.grid
+      .row
+          .ui.button(@click="setMode(1)") BTS Dark Green
+          .ui.button(@click="setMode(2)") BTS Light Green
+          .ui.button(@click="setMode(3)") MRT Blue
+          .ui.button(@click="setMode(4)") MRT Purple
+          .ui.button(@click="setMode(5)") Airport Link
+          label From To {{ getDest }}
+    // Map
+    .ui.contrainer.mapContrainer#BTSDARKGREEN(v-show="mode === 1")
+      .ui.grid
+        .row
+          .ui.circular.huge.icon.button(v-for="index in 7", @mouseover="statusText = 'ffaafafaf'", @mouseout="statusText = 'index'", @click="updateVal('ffaafafaf')")
+        .row
+          .ui.circular.huge.icon.button(v-for="index in 2")
+    .ui.contrainer.mapContrainer#BTSLIGHTGREEN(v-show="mode === 2")
+    .ui.contrainer.mapContrainer#MRTBLUE(v-show="mode === 3")
+    .ui.contrainer.mapContrainer#MRTPURPLE(v-show="mode === 4")
+    .ui.contrainer.mapContrainer#AIRPORTLINK(v-show="mode === 5")
+    // Status
+    .ui.black.fluid.label.status {{ statusText }}
+    //
       .ten.wide.column
         DestMap(v-model="destination", @selected="updateDestination")
       .six.wide.right.floated.column
@@ -16,10 +43,28 @@
 </template>
 
 <style scope>
-  .fant {
-    background-color: orange;
-    height: 700px;
+  .mapContrainer {
+    height: 550px;
     width: 100%;
+  }
+  #BTSDARKGREEN {
+    background-image: url("https://static.pexels.com/photos/17679/pexels-photo.jpg")
+  }
+  #BTSLIGHTGREEN {
+    background-color: lightgreen;
+  }
+  #MRTBLUE {
+    background-color: blue;
+  }
+  #MRTPURPLE {
+    background-color: purple;
+  }
+  #AIRPORTLINK {
+    background-color: red;
+  }
+  .steps {
+    padding-left: 0px !important;
+    padding-right: 0px !important;
   }
 </style>
 
@@ -32,10 +77,20 @@
     components: { DestForm, DestMap },
     data () {
       return {
-        destination: { from_val: '', to_val: '' }
+        destination: { from_val: '', to_val: '' },
+        steps: ['From', 'To', 'Result'],
+        current_step: 1,
+        mode: 1,
+        statusText: 'eiei'
+      }
+    },
+    computed: {
+      getDest: function () {
+        return this.destination
       }
     },
     methods: {
+      /*
       resetDestination () {
         this.destination.from_val = ''
         this.destination.to_val = ''
@@ -43,6 +98,22 @@
       },
       updateDestination: function () {
         this.$refs.destForm.setSelected(this.destination.from_val, this.destination.to_val)
+      },
+      */
+      getStepClass (index) {
+        if (index === this.current_step) {
+          return { active: true }
+        } else if (index < this.current_step) {
+          return { completed: true, link: true }
+        } else {
+          return { disabled: true }
+        }
+      },
+      setMode (index) {
+        this.mode = index
+      },
+      updateVal (value) {
+        this.destination.from_val = value
       }
     }
   }
